4b326be656efe7ef5790c005062616c6b40d79b0
Felipe Gonzalez <felipe.gonzalez@axiacore.com>
2016-09-26 16:29:21 -0500

    :sunny: It's my first day

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var browserSync = require('browser-sync');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var pug = require('gulp-pug');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('compile-sass', function() {
+    gulp.src('static/sass/*.sass')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('static/css/'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('compile-pug', function() {
+    gulp.src('static/pug/**/*.pug')
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest('public'));
+});
+
+
+gulp.task('watch', function() {
+    browserSync.init({
+        notify: false,
+        proxy: 'localhost:8000'
+    });
+
+    gulp.watch('static/sass/*.sass', ['compile-sass']);
+    gulp.watch('static/js/**/*', browserSync.reload);
+    gulp.watch('static/pug/**/*.pug', browserSync.reload);
+});
+
+gulp.task('default', ['compile-sass', 'watch', 'compile-pug']);
